a86cb1bd8daf38bc3ae98d3ca91a0a2834262482
John Jesus <john.j.jesus@gmail.com>
2021-11-14 07:46:07 -0600

    Add comments to dockerShell.sh

--- src/ubu/dockerShell.sh
+++ src/ubu/dockerShell.sh
@@ -1,8 +1,19 @@
+# Runs bash inside a base ubuntu container
+
+# Get the host ip address, something like 10.0.2.15
+#
+HOST_IP_ADDR=$(ifconfig enp0s3 | awk '/ *inet /{print $2}')
+
+# The current directory will be mounted inside the container
+# Actually, we mount the parent directory so that we have
+# access to more content
+#
+HOST_BASE_DIR=$(pwd)/../
+
 docker run \
     --name ubuntu \
-    -e HOST_IP=$(ifconfig enp0s3 | awk '/ *inet /{print $2}') \
-    -v /home/jjesus/git/try-docker/src:/src \
+    -e HOST_IP=${HOST_IP_ADDR} \
+    -v ${HOST_BASE_DIR}:/src \
     -t -i --rm \
     ubuntu /bin/bash
-
 
